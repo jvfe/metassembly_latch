bc779ac1586f6419259df840e13778b44eee0dac
jvfe <jvfecav@gmail.com>
2022-07-05 15:41:58 -0300

    feat: Add initial MEGAHIT-only version

--- wf/__init__.py
+++ wf/__init__.py
@@ -1,93 +1,121 @@
 """
-Predict protein-coding genes with prodigal
+Read assembly and evaluation for metagenomics data
 """
 
 import subprocess
 from pathlib import Path
 
-from latch import small_task, workflow
-from latch.types import LatchDir, LatchFile
+from latch import message, small_task, workflow
+from latch.types import LatchAuthor, LatchDir, LatchFile, LatchMetadata, LatchParameter
+
+METASSEMBLY_DOCS = LatchMetadata(
+    display_name="MetAssembly",
+    author=LatchAuthor(
+        name="jvfe",
+        github="https://github.com/jvfe",
+    ),
+    repository="https://github.com/jvfe/metassembly_latch",
+    license="MIT",
+)
+
+METASSEMBLY_DOCS.parameters = {
+    "read_1": LatchParameter(
+        display_name="Read 1",
+        description="Paired-end read 1 file.",
+        header="MEGAHIT",
+    ),
+    "read_2": LatchParameter(
+        display_name="Read 2",
+        description="Paired-end read 2 file.",
+        header="MEGAHIT",
+    ),
+    "k_min": LatchParameter(
+        display_name="Minimum kmer size",
+        description="Must be odd and <=255",
+        header="MEGAHIT",
+    ),
+    "k_max": LatchParameter(
+        display_name="Maximum kmer size",
+        description="Must be odd and <=255",
+        header="MEGAHIT",
+    ),
+    "k_step": LatchParameter(
+        display_name="Increment of kmer size of each iteration",
+        header="MEGAHIT",
+        description="Must be even and <=28",
+    ),
+    "min_contig_len": LatchParameter(
+        display_name="Minimum length of contigs to output",
+        header="MEGAHIT",
+    ),
+}
 
 
 @small_task
-def predict_genes(fasta: LatchFile, sample_name: str, output_format: str) -> LatchDir:
+def megahit(
+    read_1: LatchFile,
+    read_2: LatchFile,
+    sample_name: str,
+    min_count: int,
+    k_min: int,
+    k_max: int,
+    k_step: int,
+    min_contig_len: int,
+) -> LatchDir:
 
-    # A reference to our output.
-    output_dir_name = f"prodigal_results/{sample_name}/"
+    # Check input values are as expected for MEGAHit
+    if all([k_min % 2 != 0, k_max % 2 != 0, k_step % 2 == 0]) != True:
+        title = "Invalid K list selected"
+        body = "Both k min and k max have to be odd numbers, while k step must be even."
+        message(typ="error", data={"title": title, "body": body})
+
+    output_dir_name = f"MEGAHIT-{sample_name}"
     output_dir = Path(output_dir_name).resolve()
-    output_dir.mkdir(parents=True, exist_ok=True)
-
-    output_file = output_dir.joinpath(f"{sample_name}.{output_format}")
-    output_proteins = output_dir.joinpath(f"{sample_name}.faa")
-    output_genes = output_dir.joinpath(f"{sample_name}.fna")
-    output_scores = output_dir.joinpath(f"{sample_name}.cds")
-
-    _prodigal_cmd = [
-        "/root/prodigal",
-        "-i",
-        fasta.local_path,
-        "-f",
-        output_format,
-        "-o",
-        str(output_file),
-        "-a",
-        str(output_proteins),
-        "-d",
-        str(output_genes),
-        "-s",
-        str(output_scores),
+    _megahit_cmd = [
+        "megahit",
+        "--min-count",
+        min_count,
+        "--k-min",
+        k_min,
+        "--k-max",
+        k_max,
+        "--k-step",
+        k_step,
+        "--out-dir",
+        output_dir_name,
+        "--out-prefix",
+        sample_name,
+        "--min-contig-len",
+        min_contig_len,
+        "-1",
+        read_1.local_path,
+        "-2",
+        read_2.local_path,
     ]
 
-    subprocess.run(_prodigal_cmd)
+    subprocess.run(_megahit_cmd)
 
     return LatchDir(str(output_dir), f"latch:///{output_dir_name}")
 
 
-@workflow
-def prodigal(
-    fasta: LatchFile,
-    sample_name: str = "prodigal_sample",
-    output_format: str = "gbk",
+@workflow(METASSEMBLY_DOCS)
+def metagenomic_assembly(
+    read_1: LatchFile,
+    read_2: LatchFile,
+    sample_name: str = "assembly_sample",
+    min_count: int = 2,
+    k_min: int = 21,
+    k_max: int = 141,
+    k_step: int = 12,
+    min_contig_len: int = 200,
 ) -> LatchDir:
-    """Predict protein-coding genes with Prodigal
-
-    Prodigal
-    --------
-
-    Prodigal is a protein-coding gene predictor for prokaryotic genomes.
-    Read more about it [here](https://github.com/hyattpd/Prodigal).
-
-    __metadata__:
-        display_name: Prodigal (Gene prediction software)
-        author:
-            name: João Vitor Cavalcante
-            email:
-            github: https://github.com/jvfe/
-        repository: https://github.com/jvfe/prodigal-latch
-        license:
-            id: MIT
-
-    Args:
-
-        sample_name:
-          Sample name (will define output file names).
-
-          __metadata__:
-            display_name: Sample name
-
-        fasta:
-          Single/multiple FASTA input sequence.
-
-          __metadata__:
-            display_name: FASTA
-
-        output_format:
-          Specify main output file format (one of gbk, gff or sco).
-
-          __metadata__:
-            display_name: Output file format (gbk, gff or sco)
-
-    """
-    return predict_genes(
-        fasta=fasta, sample_name=sample_name, output_format=output_format
+    return megahit(
+        read_1=read_1,
+        read_2=read_2,
+        sample_name=sample_name,
+        min_count=min_count,
+        k_min=k_min,
+        k_max=k_max,
+        k_step=k_step,
+        min_contig_len=min_contig_len,
     )
